d4a6205a39e568030d209fdd36d59bf155226124
vamsi krishna <vamsimkota@gmail.com>
2020-04-01 12:58:24 +0530

    retriveing menu based on role of the user

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,4 @@
+
 function toggleSignIn() {
     var email = document.getElementById('email_id').value;
     var password = document.getElementById('password_id').value;
@@ -29,9 +30,50 @@ function toggleSignIn() {
         // based on user role navigate to differnet pages
         if(user) {
             sessionStorage.setItem("email", email);
-            window.location.href = "opentry.html"; //After successful login, user will be redirected to home.html
+            console.log("onAuthStateChanged");
+            navigate(email);
+            //window.location.href = "opentry.html"; //After successful login, user will be redirected to home.html
         }
     });
     
     document.getElementById('quickstart-sign-in').disabled = true;
+}
+//var roleRef;
+function navigate(email){
+    // retrive role of the user
+    var userRef = db.collection("user").doc(email);
+    var roleRef;
+    userRef.get().then(function(doc) {
+        if (doc.exists) {
+            roleRef = doc.data().role;
+            console.log(typeof(roleRef));
+            console.log(roleRef);
+            //retrive menu list of the user
+            var menuRef = db.collection("menu").doc(roleRef);
+            var menuList = {};
+            menuRef.get().then(function(docRef) {
+                if (docRef.exists) {
+                    //console.log(docRef.data());
+                    menuList = docRef.data();
+                    // set session object
+                    //
+                    //
+                    
+                    // for(var index in menuList) {
+                    //     console.log(menuList[index]);
+                    //   }
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+            
+        } else {
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });    
 }
