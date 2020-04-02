e2b09d1a62a67b3daacfd5a7b7c65b02ff1a0c83
vamsi krishna <vamsimkota@gmail.com>
2020-04-02 11:11:25 +0530

    dynamic menu for guest and doctor completed

--- js/auth.js
+++ js/auth.js
@@ -49,15 +49,35 @@ function navigate(email){
             // console.log(typeof(roleRef));
             // console.log(roleRef);
             //retrive menu list of the user
-            var menuRef = db.collection("menu").doc(roleRef);
-            var menuList = {};
-            menuRef.get().then(function(docRef) {
+            //var menuRef = db.collection("menu").doc(roleRef);
+            // RETRIVE MENU NAME
+            var menuNameRef = db.collection("menu").doc(roleRef).collection("name").doc("nameRef");
+            var menuNameRefList = {};
+            menuNameRef.get().then(function(docRef) {
                 if (docRef.exists) {
                     
-                    menuList = docRef.data();
-                    //console.log(menuList);
+                    menuNameRefList = docRef.data();
+                    console.log(menuNameRefList);
                     // set session object
-                    sessionStorage.setItem("menuList", JSON.stringify(menuList));
+                    sessionStorage.setItem("menuNameRefList", JSON.stringify(menuNameRefList));
+                    //window.location.href = "home.html";
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+            // RETRIVE MENU LINK
+            var menuLinkRef = db.collection("menu").doc(roleRef).collection("link").doc("linkRef");
+            var menuLinkRefList = {};
+            menuLinkRef.get().then(function(docRef) {
+                if (docRef.exists) {
+                    
+                    menuLinkRefList = docRef.data();
+                    console.log(menuLinkRefList);
+                    // set session object
+                    sessionStorage.setItem("menuLinkRefList", JSON.stringify(menuLinkRefList));
                     window.location.href = "home.html";
                 } else {
                     // doc.data() will be undefined in this case
--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,17 @@
-
 function initMenu(){
-    var menuListRef = JSON.parse(sessionStorage.menuList);
+    var menuNameRef = JSON.parse(sessionStorage.menuNameRefList);
+    var menuLinkRef = JSON.parse(sessionStorage.menuLinkRefList);
     var ul = document.createElement("ul");
     ul.classList.add("navbar-nav");
-    for(var index in menuListRef) {
+    for(var index in menuNameRef) {
         //console.log(menuListRef[index]);
         //create li tag
         var li = document.createElement("li");
         li.classList.add("nav-item");
         var a = document.createElement("a");
         a.classList.add("nav-link");
-        a.href = menuListRef[index];
-        a.innerText = index;
+        a.href = menuLinkRef[index];
+        a.innerText = menuNameRef[index];
         li.appendChild(a);
         ul.appendChild(li);
     }
